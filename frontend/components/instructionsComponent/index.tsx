75abe369f606fb144479122d121919a8f9666719
demonno <demur.nodia@gmail.com>
2023-08-27 12:16:21 +0400

    Fetch token balance for lottery token
    Address: 0x23A1B07200F9972CC0F5A4e40bb591eD37866172

--- frontend/components/instructionsComponent/index.tsx
+++ frontend/components/instructionsComponent/index.tsx
@@ -1,3 +1,4 @@
+import { useAccount, useContractRead, useNetwork } from "wagmi";
 import styles from "./instructionsComponent.module.css";
 
 export default function InstructionsComponent() {
@@ -11,5 +12,75 @@ export default function InstructionsComponent() {
 }
 
 function PageBody() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <WalletInfo></WalletInfo>
+    </div>
+  );
+}
+
+function WalletInfo() {
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const { chain } = useNetwork();
+  if (address)
+    return (
+      <div>
+        <p>Your account address is {address}</p>
+        <p>Connected to the network {chain?.name}</p>
+
+        <TokenBalance address={address}></TokenBalance>
+      </div>
+    );
+  if (isConnecting)
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  if (isDisconnected)
+    return (
+      <div>
+        <p>Wallet disconnected. Connect wallet to continue</p>
+      </div>
+    );
+  return (
+    <div>
+      <p>Connect wallet to continue</p>
+    </div>
+  );
+}
+
+function TokenBalance(params: { address: `0x${string}` }) {
+  const { data, isError, isLoading } = useContractRead({
+    address: "0x23A1B07200F9972CC0F5A4e40bb591eD37866172", // TODO fetch from env
+    abi: [
+      {
+        constant: true,
+        inputs: [
+          {
+            name: "_owner",
+            type: "address",
+          },
+        ],
+        name: "balanceOf",
+        outputs: [
+          {
+            name: "balance",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    functionName: "balanceOf",
+    args: [params.address],
+  });
+
+  const balance = typeof data === "number" ? data : 0;
+
+  if (isLoading) return <div>Fetching balance…</div>;
+  if (isError) return <div>Error fetching balance</div>;
+  return <div>Balance: {balance}</div>;
 }
