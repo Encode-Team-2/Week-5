f7f73581539796cb4244fb2d51d7a1bd6fc98904
demonno <demur.nodia@gmail.com>
2023-09-11 10:01:13 +0400

    Add contract json and token symbol component

--- frontend/components/instructionsComponent/index.tsx
+++ frontend/components/instructionsComponent/index.tsx
@@ -2,6 +2,10 @@ import { useAccount, useContractRead, useNetwork } from "wagmi";
 import styles from "./instructionsComponent.module.css";
 import { ethers } from "ethers";
 
+import TOKEN_JSON from "../../assets/LotteryToken.json";
+
+const TOKEN_ADDRESS = "0x23A1B07200F9972CC0F5A4e40bb591eD37866172";
+
 export default function InstructionsComponent() {
   return (
     <div className={styles.container}>
@@ -28,7 +32,7 @@ function WalletInfo() {
       <div>
         <p>Your account address is {address}</p>
         <p>Connected to the network {chain?.name}</p>
-
+        <TokenSymbol></TokenSymbol>
         <TokenBalance address={address}></TokenBalance>
       </div>
     );
@@ -53,7 +57,7 @@ function WalletInfo() {
 
 function TokenBalance(params: { address: `0x${string}` }) {
   const { data, isError, isLoading } = useContractRead({
-    address: "0x23A1B07200F9972CC0F5A4e40bb591eD37866172", // TODO fetch from env
+    address: TOKEN_ADDRESS, // TODO fetch from env
     abi: [
       {
         constant: true,
@@ -85,4 +89,22 @@ function TokenBalance(params: { address: `0x${string}` }) {
   if (isLoading) return <div>Fetching balance…</div>;
   if (isError) return <div>Error fetching balance</div>;
   return <div>Balance: {balanceInETH}</div>;
+}
+
+function TokenSymbol() {
+  const { data, isError, isLoading } = useContractRead({
+    address: TOKEN_ADDRESS,
+    abi: TOKEN_JSON.abi,
+    functionName: "symbol",
+  });
+
+  const name = typeof data === "string" ? data : 0;
+
+  if (isLoading) return <p>Fetching name…</p>;
+  if (isError) return <p>Error fetching name.</p>;
+  return (
+    <p>
+      Token symbol: <b>{name}</b>
+    </p>
+  );
 }
